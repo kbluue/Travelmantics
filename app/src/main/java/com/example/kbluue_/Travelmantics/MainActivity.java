04d81868e55e0927b4d1f62860679890f51de109
_kbluue_ <kbluue@yahoo.com>
2019-08-04 21:04:36 +0100

    LAOListener => Retriever

--- app/src/main/java/com/example/kbluue_/Travelmantics/MainActivity.java
+++ app/src/main/java/com/example/kbluue_/Travelmantics/MainActivity.java
@@ -1,16 +1,12 @@
 package com.example.kbluue_.Travelmantics;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.example.kbluue_.Travelmantics.DatabaseObjects.Deal;
-import com.example.kbluue_.Travelmantics.Utils.DatabaseUtils;
-
-import java.util.Set;
-
-import static android.util.Log.wtf;
-import static com.example.kbluue_.Travelmantics.R.string.app_name;
+import com.example.kbluue_.Travelmantics.Activities.ChooseSignInActivity;
+import com.example.kbluue_.Travelmantics.Activities.ViewDealsActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,21 +15,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Set<Deal> deals = DatabaseUtils.getAllObject("", Deal.class);
-        wtf(getString(app_name), "MainActivity.onCreate: start" + deals.size());
-        Deal[] deals1 = new Deal[deals.size()];
-        deals1 = deals.toArray(deals1);
-        for (int i = 0; i < deals.size(); i++) {
-            wtf(getString(app_name), "MainActivity.onCreate: " + deals1[i]);
-            wtf(getString(app_name), "MainActivity.onCreate: " + deals1[i].getDesc());
+        if (isNew()){
+            startActivity(new Intent(this, ChooseSignInActivity.class));
+        } else {
+            startActivity(new Intent(this, ViewDealsActivity.class));
         }
-        wtf(getString(app_name), "MainActivity.onCreate: end");
-
-//        if (isNew()){
-//            startActivity(new Intent(this, ChooseSignInActivity.class));
-//        } else {
-//            startActivity(new Intent(this, ViewDealsActivity.class));
-//        }
     }
 
     private boolean isNew(){
