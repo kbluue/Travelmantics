b386db44d2f8abef663c275dc63bc58b015fcb6d
_kbluue_ <kbluue@yahoo.com>
2019-08-09 05:53:16 +0100

    AuthUtils: Chrome crash plug

--- app/src/main/java/com/example/kbluue_/Travelmantics/Firebase/AuthUtils.java
+++ app/src/main/java/com/example/kbluue_/Travelmantics/Firebase/AuthUtils.java
@@ -1,4 +1,31 @@
 package com.example.kbluue_.Travelmantics.Firebase;
 
-public class AuthUtils {
+import android.app.Activity;
+
+import com.firebase.ui.auth.AuthUI;
+import com.google.firebase.auth.FirebaseAuth;
+
+import java.util.Arrays;
+import java.util.List;
+
+public interface AuthUtils {
+
+    int RC_SIGN_IN = 555;
+
+    static void signIn(Activity host){
+        List<AuthUI.IdpConfig> providers = Arrays.asList(
+                new AuthUI.IdpConfig.EmailBuilder().build(),
+                new AuthUI.IdpConfig.GoogleBuilder().build());
+
+        host.startActivityForResult(
+                AuthUI.getInstance()
+                        .createSignInIntentBuilder()
+                        .setAvailableProviders(providers)
+                        .build(),
+                RC_SIGN_IN);
+    }
+
+    static boolean isLoggedIn(){
+        return FirebaseAuth.getInstance().getCurrentUser() != null;
+    }
 }
--- app/src/main/java/com/example/kbluue_/Travelmantics/MainActivity.java
+++ app/src/main/java/com/example/kbluue_/Travelmantics/MainActivity.java
@@ -2,11 +2,16 @@ package com.example.kbluue_.Travelmantics;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.example.kbluue_.Travelmantics.Activities.ChooseSignInActivity;
-import com.example.kbluue_.Travelmantics.Activities.ViewDealsActivity;
+import com.example.kbluue_.Travelmantics.Firebase.AuthUtils;
+import com.firebase.ui.auth.IdpResponse;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import static com.example.kbluue_.Travelmantics.Firebase.AuthUtils.RC_SIGN_IN;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,10 +20,37 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        if (isNew()){
-            startActivity(new Intent(this, ChooseSignInActivity.class));
+        if (AuthUtils.isLoggedIn()){
+
         } else {
-            startActivity(new Intent(this, ViewDealsActivity.class));
+            AuthUtils.signIn(this);
+        }
+
+//        if (isNew()){
+//            startActivity(new Intent(this, ChooseSignInActivity.class));
+//        } else {
+//            startActivity(new Intent(this, ViewDealsActivity.class));
+//        }
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == RC_SIGN_IN) {
+            IdpResponse response = IdpResponse.fromResultIntent(data);
+
+            if (resultCode == RESULT_OK) {
+                // Successfully signed in
+                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                Toast.makeText(this, "Signed in", Toast.LENGTH_SHORT).show();
+                // ...
+            } else {
+                // Sign in failed. If response is null the user canceled the
+                // sign-in flow using the back button. Otherwise check
+                // response.getError().getErrorCode() and handle the error.
+                // ...
+                Toast.makeText(this, "failed", Toast.LENGTH_SHORT).show();
+            }
         }
     }
 
